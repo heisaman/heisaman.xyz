e763551e112bc5a09bc078e374e12ae753e43746
Heisaman <heyuwei.efrei@gmail.com>
2016-10-10 14:48:18 +0000

    remove useless columns

--- regular_tasks.sh
+++ regular_tasks.sh
@@ -1,4 +1,4 @@
-mongoexport --db weibo --collection favorites --out ~/freshman21/_data/favorites.json --jsonArray
+mongoexport --db weibo --collection favorites --sort '{"created_at": -1}' --out ~/freshman21/_data/favorites.json --jsonArray
 cd /usr/share/nginx/html/
 rm -rf ./*
 jekyll build --source ~/freshman21/ --destination ./
