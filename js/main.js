d53494954c4adc0a4b5de9b0d1b44b424f0ff57e
Heisaman <heyuwei.efrei@gmail.com>
2016-12-24 16:25:30 +0800

    fix jquery $ not defined error

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    var autolinker = new Autolinker( {
+        urls : {
+            schemeMatches : true,
+            wwwMatches    : true,
+            tldMatches    : true
+        },
+        email       : true,
+        phone       : true,
+        mention     : 'weibo',
+        hashtag     : false,
+
+        stripPrefix : true,
+        newWindow   : true,
+
+        truncate : {
+            length   : 0,
+            location : 'end'
+        },
+
+        className : ''
+    } );
+
+    $('.WB_text').each(function(i, obj) {
+        $(obj).innerHTML = autolinker.link( $(obj).innerHTML );
+    });
+});
